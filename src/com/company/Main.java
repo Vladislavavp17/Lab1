b0d175d66fe40e827254a183ac7c7e63fbd346a7
SuckRat_Us <suckrat.us1337@gmail.com>
2018-09-23 17:19:08 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Владислава";
+        String surname = "Пьяных";
+        String group = "AT-161";
+
+        System.out.println("Меня зовут " + name + " " + surname);
+        System.out.println("Я студентка группы " + group);
+    }
+}
